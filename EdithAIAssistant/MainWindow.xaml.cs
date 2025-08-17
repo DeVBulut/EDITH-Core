dea3ddfac073b1b6d2e69c3382bf6bfa20e8afe5
Emirhan <emirhanbuluttech@gmail.com>
2025-08-17 02:50:29 -0600

    C# Upload and gitignore changes
    Swapped the priject to C#
    Created a basic UI
    Updated gitignore.

---
+++ EdithAIAssistant/MainWindow.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace EdithAIAssistant
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void MinimizeButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void Window_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            // Enable dragging the window from anywhere
+            if (e.ButtonState == MouseButtonState.Pressed)
+                DragMove();
+        }
+    }
+}
